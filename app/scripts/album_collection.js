2db6a6f76d7b07c9a0be9e108cbb809d417c980f
phitchcock <phitchcock@me.com>
2014-04-01 09:50:28 -0700

    removed overlay function

--- app/scripts/album_collection.js
+++ app/scripts/album_collection.js
@@ -3,12 +3,12 @@ var createAlbumRow = function() {
 
   var template = 
       '<div class="col-md-2">'
-    + ' <div class="thumbnail [...]">'
-    + '   <div class="collection-album-image-container [...]">'
+    + ' <div class="thumbnail">'
+    + '   <div class="collection-album-image-container">'
     + '     <img src="/images/album-placeholder.png">'
     + '   </div>'
     + ' </div>'
-    + ' <div class="caption [...]">'
+    + ' <div class="caption">'
     + '   <a href="/album" class="album-title-collection">Album Name</a>'
     + '   <br>'
     + '   <a href="/album" class="album-artist collection">Artist Name</a>'
@@ -47,47 +47,16 @@ var updateCollectionView = function() {
 
 };
 
-var overlayView = function() {
-
-   var onHover = function(event) {
-    // Add the overlay to image container.
-    //  - 'this' is a reference to the object whose `hover` action was triggered for.
-    //  - '$(this)' returns the jQuery wrapped object for the element that was hovered over.
-      $(this).append(buildAlbumOverlay("/album"));
-    };
-
-    // Function that will be called whenever an album image is hovered over.
-    var offHover = function(event) {
-    // Remove the overlay from the album image container.
-      $(this).find('.collection-album-image-container').remove();
-    };
-
-    // for loop that builds your thumbnails.
-    for (var i = 0; i < 33; i++) {
-
-      var $newThumbnail = buildAlbumOverlay('/album');
-
-
-      // Code to build album thumbnail.;
-      // Find the image container in the thumbnail and add a hover event handler with
-      //  our event handlers for on/off hover.
-      //  - The jQuery documentation: http://api.jquery.com/hover/
-      $newThumbnail.find('.collection-album-image-container').hover(onHover, offHover);
-      $('.collection-album-image-container').append($newThumbnail);
-    };
-
-};
 
 var overlay = function() {
 
-  $( ".collection-album-image-container" ).hover(
-  function() {
-    $( this ).append( buildAlbumOverlay('/album'));
-  }, function() {
-    $( this ).find( ".collection-album-image-overlay" ).remove();
-  }
-);
-
+  $('.collection-album-image-container').hover(
+    function() {
+      $(this).append(buildAlbumOverlay('/album'));
+    }, function() {
+      $(this).find('.collection-album-image-overlay').remove();
+    }
+  );
 
 };
 
@@ -97,9 +66,7 @@ if (document.URL.match(/\/collection/)) {
   $(document).ready(function() {
 
      updateCollectionView();
-     //overlayView();
      overlay();
-
     
   }); //document.ready
 } // if
