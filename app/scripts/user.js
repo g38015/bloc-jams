3491288f91f4323f30fc5e68fe1e36711898d01f
phitchcock <phitchcock@me.com>
2014-04-04 11:37:36 -0700

    working solution for tabs

--- app/scripts/user.js
+++ app/scripts/user.js
@@ -1,17 +1,38 @@
+
+var tabsContainerName = ".user-profile";
+var changeTabHandler = function(event) {
+  $this = $(this);
+  $(tabsContainerName + " button.active").removeClass('active');
+  $this.addClass('active');
+  $('.tab-pane').addClass('hidden');
+  selectedTabId = $this.find('a').attr('data-toggle');
+  $("#" + selectedTabId).removeClass('hidden');
+  event.preventDefault();
+};
+
 $(document).ready(function() {
 
-  $('#home').show();
+  var $tabs = $(tabsContainerName + " .pagination button");
+  $tabs.click(changeTabHandler);
+  $tabs[0].click();
 
-  var changeDiv = $('#home');
-  var changeCards = $('#cards');
-  
-  $('#btn-home').click(function() {
-    $('#empty').html(changeDiv);
-  });
 
-  $('#btn-cards').click(function() {
-    $('#empty').html(changeCards);
-  });
+  // $('#home').show();
+
+  // var changeDiv = $('#home');
+  // var changeCards = $('#cards');
+
+  // $('#btn-home').click(function() {
+  //   changeDiv.addClass('hidden');
+  //   changeCards.removeClass('hidden');
+  //   //$('#empty').html(changeDiv);
+  // });
+
+  // $('#btn-cards').click(function() {
+  //   //$('#empty').html(changeCards);
+  //   changeCards.addClass('hidden');
+  //   changeDiv.removeClass('hidden');
+  // });
 
 
 });
